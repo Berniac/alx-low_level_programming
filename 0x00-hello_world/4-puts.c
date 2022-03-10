e496f09f9826fcf0224ac66e9db17cc1ce7f308d
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-10 10:53:45 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 
 #include <stdio.h>
 
-/*
+/**
  * Prints "Programming is like building a multilingual puzzle
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
