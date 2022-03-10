0d8d5bb563ea5c613026e112ad55ace07de8c893
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-10 11:35:58 +0300

    Size of types
     Prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /*
- * prints the size of varous types on the computer it is compiled and run on
+ * prints the size of various types on the computer it is compiled and run on
  *
  */
 int main(void)
@@ -13,6 +13,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
