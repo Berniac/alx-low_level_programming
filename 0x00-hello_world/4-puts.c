f0fe3629cba8c24600d9b6aa8ebd50c9f1ec825c
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-10 10:50:01 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-/* Print out "Programming is like building a multilingual puzzle */
+/*
+ * Prints "Programming is like building a multilingual
+ *                puzzle.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
