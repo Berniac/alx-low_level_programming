cf5aed2f6976b6700b68781137178e28b02d9300
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-10 20:43:52 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types on the computer 
- * it is compiled and run on
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
