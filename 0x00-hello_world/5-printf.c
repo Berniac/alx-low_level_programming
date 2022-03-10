2ec82fd37c5344026058e3ecc6aa604f1bf3918c
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-10 20:35:45 +0300

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
+/*
+ * File: 5-printf.c
+ */
+
 #include <stdio.h>
 
 /**
- * main - Prints 'with proper grammar, but the outcome is a piece of art,'
+ * main - Prints 'with proper grammar, but the outcome 
+ * is a piece of art,'
+ *
+ * return: always 0
  */
 
 int main(void)
