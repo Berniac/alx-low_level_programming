aa1336a108858ae00160bd152372a370aac9b9de
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-11 15:45:12 +0300

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,11 @@ int main(void)
 	char letter;
 	int num;
 
-	for(num = 0; num < 10; num++){
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	}
 
-	for(letter = 'a'; letter <= 'f'; letter++){
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-	}
 
 	putchar('\n');
 
