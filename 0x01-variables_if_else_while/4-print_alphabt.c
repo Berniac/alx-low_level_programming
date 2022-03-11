ebcbc6227e7e063addb538cf5c4578449b19f6ee
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-11 15:32:56 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,9 @@ int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++){
-		if(letter != 'e' && letter != 'q'){
+	for (letter = 'a'; letter <= 'z'; letter++) {
+		if (letter != 'e' && letter != 'q')
 			putchar(letter);
-		}
 	}
 
 	putchar('\n');
