df58bc753bf08ca553e363594888b00a7503ad89
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-11 15:47:23 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int num;
 
-	for(num = 0; num < 10; num++){
+	for (num = 0; num < 10; num++)
+	{
 		putchar((num % 10) + '0');
-		if (num == 9){
+		if (num == 9)
 			continue;
-		}
+		
 		putchar(',');
 		putchar(' ');
 	}
