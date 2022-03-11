0e6c040ed4da73b778d2d0ede2e897dec38c68aa
Berniac <bwadero@gmail.com>
2022-03-11 13:19:28 -0800

    2 two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of 2 2-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k, l, op1, op2;
+
+	i = j = k = l = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			k = 48;
+			while (k < 58)
+			{
+				l = 48;
+				while (l < 58)
+				{
+					op1 = (i * 10) + j;
+					op2 = (k * 10) + l;
+					if (op1 < op2)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+						if (i == 57 && j == 56 && k == 57 && l == 56)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
