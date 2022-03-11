b1b1f648445584550335af1232e8deb066c2f27b
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-11 15:22:14 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	
+
 	putchar('\n');
 
 	return (0);
