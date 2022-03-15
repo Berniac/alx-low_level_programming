3d541d7ee81993ea5453a24be8c39fadc1c955a3
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-15 16:54:52 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  *
- * @n - number
+ * @n: number
  *
  * Return: 1 if greater than 0, 0 if zero,
  * -1 otherwise.
@@ -14,25 +14,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 
 	_putchar('\n');
 }
-
