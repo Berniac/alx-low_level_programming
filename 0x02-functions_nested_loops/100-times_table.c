8b0ae66a65115e5ad263b9cfc215aa0532ce5806
Berniac <bwadero@gmail.com>
2022-03-15 12:01:23 -0700

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n timetable
+ *
+ * @n: input number
+ *
+ * Return: Void
+ */
+void print_times_table(int n)
+{
+	int i, j, op;
+
+	if (n !< 0 || n !> 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				op = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (op <= n)
+				{
+					_putchar(' ');
+					_putchar(op + 48);
+				}
+				else
+				{
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
