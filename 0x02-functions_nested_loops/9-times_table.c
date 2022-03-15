16d1c5a36b21eac46c9dd2062fd594ac88dfa5e3
Berniac <bwadero@gmail.com>
2022-03-15 10:26:12 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,22 @@ void times_table(void)
 
 	for  (i = 0; i <= 9; i++)
 	{
+		_putchar(48);
 		for (j = 0; j <= 9; j++)
 		{
 			prod = i * j;
-			_putchar(prod);
+			_putchar(',');
 			_putchar(' ');
+			if (prod <= 9)
+			{
+				_putchar(' ');
+				_putchar(prod + 48);
+			}
+			else
+			{
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
 		}
 
 		_putchar('\n');
