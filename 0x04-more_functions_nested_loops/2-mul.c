032d38799cfc927033766fa530fab961c7031341
Berniac <bwadero@gmail.com>
2022-03-17 13:40:23 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c - input
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
