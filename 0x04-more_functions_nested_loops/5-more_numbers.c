dc591e03481fe7a834a3a71690eea12f6d506fe1
Berniac <bwadero@gmail.com>
2022-03-17 14:20:42 -0700

    Numbers constitute the only universal linguo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,10 @@ void more_numbers(void)
 	for (i = 0; i < 11; i ++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
-		_putchar('\n');
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
 	}
 	_putchar('\n');
 }
