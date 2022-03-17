2d5229adfa229579244ef3442b6b68d0c3725d97
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-18 01:26:35 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 || i != 52)
+		if (i != 50 && i != 52)
 			_putchar(i);
 	}
 	_putchar('\n');
