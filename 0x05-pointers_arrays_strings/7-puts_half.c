a500a32310f05c1ceec1c35fc804a387602c93ce
Berniac <bwadero@gmail.com>
2022-03-21 10:54:08 -0700

    Half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ * Return: Void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (len >= 0)
+	{
+		if (str[len] != '\0'')
+			len++;
+	}
+
+	if (len % 2 == 1)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+
+	for (i++; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
