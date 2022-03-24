dd5f08f5d0ef60786026e2a18129d157f000ed48
Berniac <bwadero@gmail.com>
2022-03-24 02:59:29 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ * @s: string
+ * Return: pointer to s
+ */
+char *string_toupper(char *s)
+{
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+	return (s);
+}
