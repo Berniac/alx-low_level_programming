5742f63481cf13a965923106e287bb7ab4338240
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-26 13:05:50 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,8 +24,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	c1--; c2--; size_r--;
+	dr1 = *(n1 + c1) - 48; dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -45,7 +45,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
-		bg--, size_r--;
+		bg--; size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
