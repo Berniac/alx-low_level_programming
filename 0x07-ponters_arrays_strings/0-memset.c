a3df9cbda753b81f367b43fed6d11052d6fa1b4c
Berniac <bwadero@gmail.com>
2022-03-28 17:37:28 -0700

    memset

---
+++ 0x07-ponters_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: The memory area
+ * @b: Constant byte
+ * @n: filled bytes
+ * Return: Pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
---
+++ 0x07-ponters_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memeset(char *s, char b, unsigned int n);
+
+#endif
