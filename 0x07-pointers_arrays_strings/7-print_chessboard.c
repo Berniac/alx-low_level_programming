ee725dc0467e9aa00e8af0488dfaa410142ed223
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-29 05:04:01 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j = 0;
+	unsigned int i, m = 0;
 
 	for (i = 0; i < 64; i++)
 	{
 		if (i % 8 == 0 && i != 0)
 		{
-			j = i;
+			m = i;
 			_putchar('\n');
 		}
 		_putchar(a[i / 8][i - m]);
