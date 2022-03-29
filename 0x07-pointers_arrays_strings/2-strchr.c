5a19e0f23e98ceec18802606e491d029bd176bf4
BernieW <87444402+Berniac@users.noreply.github.com>
2022-03-29 04:19:33 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,8 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	for (i = 0; *(s + i) != '\0'; i++)
-	{
 		if (*(s + i) == c)
 			return (s + i);
-	}
 	if (*(s + i ) == c)
 		return (s + i);
 
