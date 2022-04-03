ac1d0e09f616be1390d2a6d8dd49ef36fc0cb708
Berniac <bwadero@gmail.com>
2022-04-03 03:19:11 -0700

    Number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: number of arguments passed
+ * @argv: array of command line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return(0);
+}
