679ac3e0fb4aa04922fee4618f60d2c3e4856a7b
Berniac <bwadero@gmail.com>
2022-04-03 03:29:55 -0700

    Argument names

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * argc: number of arguments passed
+ * argv: array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
