c7ebd5edec26545a026744f7454a92cfe8d7e187
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-03 13:12:43 +0300

    Program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: command line arguments array
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
