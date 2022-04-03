2cd7a50ed6df274c1f890c552201d1f91aa1452d
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-03 13:24:11 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  * @argv: array of command line arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return(0);
 }
