79182d48f047c6cce8916a259869fc4093a101a7
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-05 23:18:14 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2-D grid
+ * ch_free_grid - frees a 2-D grid
  *
  * @grid: 2-D array of integers
  * @height: Height of the grid
  * Return: Void.
  */
-void free_grid(int **grid, int height)
+void free_grid(char **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
 		for (; height >= 0; height--)
 			free(grid[height]);
+		free(grid[height]);
 		free(grid);
 	}
 }
