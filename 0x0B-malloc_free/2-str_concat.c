de8a6e995e71c4580fb036ef5ad8f3c422af33f5
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-05 22:34:06 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2[i] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	jstr = malloc(sizeof(char) * (i + j + 1));
