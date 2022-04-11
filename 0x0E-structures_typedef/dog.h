1a70273e37a6b074b6d7ff87fc1775c922448b12
Berniac <bwadero@gmail.com>
2022-04-11 14:44:50 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - General information about a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner of the dog
+ *
+ * Description: Struct that contains information
+ * regarding the name of a dog, age and its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
