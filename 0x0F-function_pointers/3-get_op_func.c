cc728063357580d833558361c43ff06c686dd7d8
Berniac <bwadero@gmail.com>
2022-04-13 06:41:00 -0700

    Basic calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Selects correct function to
+ * perform operation asked by user
+ * @s: Operator
+ *
+ * Return: Pointer to the function corresponding
+ * to the operator given as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+
+	return (ops[i / 2].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds 2 integers
+ * @a: 1st Integer
+ * @b: 2nd Integer
+ *
+ * Return: Addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: Subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplie 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: Multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: Integer division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b)
+}
+
+/**
+ * op_mod - Gets remainder of division of 2 ints
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: Remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
