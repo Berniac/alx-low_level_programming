c59771ecea7de2e0f43255a600d12701a2b85705
Berniac <bwadero@gmail.com>
2022-04-13 05:50:18 -0700

    Index no.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (size <= 0)
 			return (-1);
 
-		for(; i < size; i++)
+		for (; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
 	}
