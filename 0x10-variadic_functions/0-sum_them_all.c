bbc5d25c1a6bdb72ec6f582b395bb73bd570eb0e
Berniac <bwadero@gmail.com>
2022-04-14 02:43:53 -0700

    Sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,10 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list ptr;
 
-	va_start(ptr, n);
-	
 	if (n == 0)
 		return (0);
+
+	va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
