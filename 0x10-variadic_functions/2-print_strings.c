32b0e3b6e5ae11897e3f941e31847be4bde8db7c
BernieW <87444402+Berniac@users.noreply.github.com>
2022-04-14 22:28:26 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
-	print("\n");
+	printf("\n");
 
 	va_end(ptr);
 }
